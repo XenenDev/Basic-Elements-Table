72f4cd4cc5b25927933ea082e91b2c886067d23e
Xander vP <65053730+XenenDev@users.noreply.github.com>
2024-09-19 11:26:29 +0700

    Removed File Path Info

--- elements.py
+++ elements.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 client = OpenAI(api_key="")
 
 
-with open("C:/Users/xande/Downloads/elements.json", 'r', encoding='utf-8') as f:
+with open("/elements", 'r', encoding='utf-8') as f:
     elements = json.loads(f.read())["elements"]
 
 new_elements = {}
@@ -27,5 +27,5 @@ for element in elements:
     print(cost, hint)
     new_elements[element.get('name')] = {'symbol': element.get('symbol'), 'number': element.get('number'), 'atomic_mass': element.get('atomic_mass'), 'hint': hint}
 
-with open("C:/Users/xande/Downloads/new_elements.json", 'w', encoding='utf-8') as f:
+with open("/new_elements.json", 'w', encoding='utf-8') as f:
     f.write(json.dumps(new_elements))
